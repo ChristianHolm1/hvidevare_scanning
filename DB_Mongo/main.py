cedac8dffb9b7e42b805d5b82b76f380fcfb5297
Christian H <10434637@ucn.dk>
2023-11-07 10:47:06 +0100

    Updatedet get stores/store_name

--- DB_Mongo/main.py
+++ DB_Mongo/main.py
@@ -1,9 +1,9 @@
 from typing import Union
 import json
 import motor.motor_asyncio
-
+import pprint
 from fastapi import FastAPI
-
+from bson import ObjectId
 app = FastAPI()
 
 config = open('config.json')
@@ -13,23 +13,24 @@ connectionString = key['mongo_connection_string']
 client = motor.motor_asyncio.AsyncIOMotorClient(connectionString)
 db = client['webstore']
 
-cur = db['elgiganten'].find_one({"title":"Samsung WW5000T vaskemaskine WW85TA047AE"})
 
-for doc in cur:
-    print(doc)
-
-
-
-
+def convert_object_ids_to_strings(doc):
+    for key, value in doc.items():
+        if isinstance(value, ObjectId):
+            doc[key] = str(value)
+    return doc
 
 # Get, Post, Delete
 @app.get("/stores")
 async def read_store():
-    return {"All stores"}
+    return {"stores": "stores"}
+    
 
 @app.get("/stores/{store_name}")
 async def read_store(store_name: str):
-    return {"store_name": store_name}
+    documents = db[store_name].find({})
+    products = [convert_object_ids_to_strings(document) for document in await documents.to_list(length=1000)]
+    return {"products": products}
 
 @app.post("/stores/{store_name}")
 async def post_store(store_name: str):
@@ -38,3 +39,4 @@ async def post_store(store_name: str):
 @app.delete("/stores/{store_name}")
 async def delete_store(store_name: str):
     return {"store_name": store_name}
+
