909cf02f8f4f602c9523565b33ea9b85a470a8ea
LRHarboe <10434646@ucn.dk>
2023-11-20 19:33:37 +0100

    Delete unnecessary files and code

--- API_Puppeteer/src/domain/Interfaces/ProductIF.ts
+++
@@ -1,8 +0,0 @@
-export interface ProductIF {     
-    title: string;
-    price: string;
-    rating?: string | 0;
-    productImage: string;
-    productLink: string;
-    productEnergyImg: string;
-}
--- API_Puppeteer/src/domain/Interfaces/ScraperIF.ts
+++
@@ -1,7 +0,0 @@
-import { Product } from "../entities/Product";
-
-export interface ScraperIF {
-    initialize(): Promise<void>;
-    scrapeProducts(website: string): Promise<Product[]>;
-    close(): Promise<void>;
-}
