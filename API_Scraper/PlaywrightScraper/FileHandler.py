b20753d5506595c044fd4279e05d4427a2543522
Christian H <10434637@ucn.dk>
2023-12-19 01:25:02 +0100

    Small refactor and added documentation

--- API_Scraper/PlaywrightScraper/FileHandler.py
+++ API_Scraper/PlaywrightScraper/FileHandler.py
@@ -2,30 +2,53 @@ import os
 import json
 
 class FileHandler:
+    """
+    Class for handling files.
+    """
     
     @staticmethod
-    def load_json(filename):
+    def save_json(data, file_path : str):
+        """
+        Save data to JSON file.
+        
+        Args:
+            data (dict): Data to save.
+            file_path (str): Path to JSON file.
+        """ 
         try:
-            with open(filename, 'r') as file:
-                print(f"Data loaded from {filename}")
-                return json.load(file)
+            with open(file_path, 'w') as file:
+                json.dump(data, file, indent=4)
+                print(f"Data saved to {file_path}")
         except Exception as e:
             print(e)
             
     @staticmethod
-    def save_json(data, filename):
+    def load_json(file_path : str):
+        """
+        Load data from JSON file.
+        
+        Args:
+            file_path (str): Path to JSON file.
+        """
         try:
-            with open(filename, 'w') as file:
-                json.dump(data, file, indent=4)
-                print(f"Data saved to {filename}")
+            with open(file_path, 'r') as file:
+                print(f"Data loaded from {file_path}")
+                return json.load(file)
         except Exception as e:
             print(e)
+      
 
     @staticmethod
-    def delete_images_from_folder(path_to_images):
+    def delete_images_from_folder(path_to_images : str):
+        """
+        Load data from JSON file.
+        
+        Args:
+            path_to_images (str): Path to folder with images.
+        """ 
         try:
-            for filename in os.listdir(path_to_images):
-                if os.path.isfile(os.path.join(path_to_images, filename)):
-                    os.remove(os.path.join(path_to_images, filename))
+            for file_path in os.listdir(path_to_images):
+                if os.path.isfile(os.path.join(path_to_images, file_path)):
+                    os.remove(os.path.join(path_to_images, file_path))
         except Exception as e:
             print(e)
--- API_Scraper/PlaywrightScraper/MLDataHandler.py
+++ API_Scraper/PlaywrightScraper/MLDataHandler.py
@@ -3,71 +3,96 @@ import os
 import requests
 
 class MLDataHandler:
-    def __init__(self, ml_api_url, path_to_images):
+    """
+    Class for handling data to machine learning API.
+    """
+    def __init__(self, ml_api_url: str, path_to_images: str):
+        """
+        Initialize the MLDataHandler class.
+        
+        Args:
+            ml_api_url (str): The URL to the machine learning API.
+            path_to_images (str): The path to the folder with images.
+        """
         self.ML_API_URL = ml_api_url
         self.path_to_images = path_to_images
         
-    def send_data_to_ML_API(self, picture_start, picture_end):
-
+    def send_data_to_ML_API(self, picture_start: int, picture_end: int):
+        """
+        Send data to the machine learning API.
+        
+        Args:
+            picture_start (int): The start index of the images to send.
+            picture_end (int): The end index of the images to send.
+        Returns:
+            dict: The result from the machine learning API.
+        """
         try:
-            # Create a list to store the files data
             files_data = []
-
-            # Iterate over the files in the specified folder
+            
             for file_name in os.listdir(self.path_to_images):
                 file_path = os.path.join(self.path_to_images, file_name)
-
-                # Ensure the item is a file (not a subdirectory)
+                
                 if os.path.isfile(file_path):
-                    # Append each file as a tuple to the files_data list
                     files_data.append(('images', (file_name, open(file_path, 'rb'))))
             files_data = files_data[picture_start:picture_end]
-            # Check if there are any files to send
+
             if not files_data:
                 print("No files found in the specified folder.")
                 return
             
-
-            # Send the files to the API using a POST request
             response = requests.post(self.ML_API_URL, files=files_data)
-
-            # Check the response status
             response.raise_for_status()
-
 
         except requests.exceptions.HTTPError as http_err:
             print(f"HTTP error occurred: {http_err}")
+            
         except requests.exceptions.RequestException as req_err:
             print(f"Request error occurred: {req_err}")
+            
         finally:
-            # Close the files
             for file_tuple in files_data:
                 file_tuple[1][1].close()
+        
         return response.json()
 
-    def insert_label_on_data(self, data, result_from_ML):
+    def insert_label_on_data(self, data: dict, result_from_ML: dict):
+        """
+        Insert the label into the data.
+        
+        Args:
+            data (dict): The data to insert the label into.
+            result_from_ML (dict): The result from the machine learning API.
+        Returns:
+            dict: The data with the label inserted.
+        """
         new_data = []
         for item in data:
             title = item['title']
-            
-            # Check if the title exists in any of the dictionaries in result_from_ML
-            
+
             for ml_item in result_from_ML:
                 if title in ml_item:
-                    # Extract the value for the title from the matching dictionary
+                    # Get's the label from the result
                     label = ml_item[title][0]
-                    # Add a new field 'energy_label' to the product
                     item['productLabel'] = label
                     new_data.append(item)
         
         return new_data
 
-    def send_to_ML_API_and_split_elements_in_chunks(self, scraped_data):
-        estimated_time = self.machine_learning_time_estimator(len(scraped_data))
+    def send_to_ML_API_and_split_elements_in_chunks(self, data: dict):
+        """
+        Send data to machine learning API and split the data into chunks of 1000 elements.
+        
+        Args:
+            scraped_data (dict): The data to send to the machine learning API.
+        Returns:
+            dict: The result from the machine learning API.
+        """
+        estimated_time = self.machine_learning_time_estimator(len(data))
         print(f'Sending data to machine learning API begun, estimated time: {estimated_time} minutes')
         
         result_from_ML = []
-        scraped_data_length = len(scraped_data)
+        scraped_data_length = len(data)
         thousands_in_scraped_data = math.floor(scraped_data_length // 1000)
 
         for i in range(thousands_in_scraped_data + 1):
@@ -79,11 +104,26 @@ class MLDataHandler:
         print('Sending data to ML API finished')
         return result_from_ML
     
-    def machine_learning_time_estimator(self, scarped_data_length):
+    def machine_learning_time_estimator(self, data_length: int):
+        """
+        Estimate the time it will take to send data to the machine learning API.
         
-        return math.ceil(scarped_data_length / 1000 * 1.5) 
+        Args:
+            data_length (int): The length of the scraped data.
+        Returns:
+            int: The estimated time in minutes.
+        """
+        return math.ceil(data_length / 1000 * 1.5) 
     
-    def process_data_to_ml(self, scraped_data):
-        result_from_ML = self.send_to_ML_API_and_split_elements_in_chunks(scraped_data)
-        result_with_lables = self.insert_label_on_data(scraped_data, result_from_ML)
+    def process_data_to_ml(self, data: dict):
+        """
+        Process data to machine learning API.
+        
+        Args:
+            data (dict): The data to process with the machine learning API.
+        Returns:
+            dict: The data with the label inserted.
+        """
+        result_from_ML = self.send_to_ML_API_and_split_elements_in_chunks(data)
+        result_with_lables = self.insert_label_on_data(data, result_from_ML)
         return result_with_lables
